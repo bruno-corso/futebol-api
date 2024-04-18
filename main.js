47d4d81a22d75fc4c8542f47c49db723273d2cff
bruno-corso <brunotcorso@gmail.com>
2024-04-17 23:59:52 -0300

    ajuste lista e busca workng

--- main.js
+++ main.js
@@ -32,13 +32,30 @@ async function buscarTime(input) {
 function criarLista(arrayTimes) {
     const listaHtml = document.getElementById('listaTimes')
     if (arrayTimes.length == 0) {
-        listaHtml.innerHTML = 
-        `<li class="list-group-item list-group-item-action">
+        listaHtml.innerHTML =
+            `<li class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1 h6">Nada foi encontrado...</h5>
             </div>
         </li>`
     }
+    else {
+        listaHtml.innerHTML = criacaoLista(arrayTimes)
+    }
+}
+
+function criacaoLista(array) {
+    const lista = array.map((item) => {
+        return (`<li class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-start align-items-center">
+            <img src=${item.team.logo} alt="logo time" style="width: 60px; height: 60px;">
+            <h5 class="mb-1 ms-3 h5">${item.team.name}</h5>
+        </div>
+        <p class="mb-1">País: ${item.team.country}</p>
+        <small class="text-body-secondary">${item.team.code}</small>
+        </li>`)
+    })
+    return lista.join("")
 }
 
 async function play() {
